3c608fbea73180ec6e2aa955633a623830adef66
97wgl <1475693887@qq.com>
2017-08-02 10:47:47 +0800

    wgl_02

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,21 +1,47 @@
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('/home/wgl/文档/tw-movie-theater-master/database/movieonline.db',function() {
-            db.all("select * from Movies",function(err,res){
-                if(!err)
-                    console.log(res);
-                else
-                    console.log(err);
-            });
-            db.all("select * from MovieComment WHERE MovieId='AQ000001'",function(err,res){
-                if(!err)
-                    console.log(res);
-                else
-                    console.log(err);
-            });
-            db.all("select * from MovieDetails WHERE MovieId='AQ000001'",function(err,res){
-                if(!err)
-                    console.log(res);
-                else
-                    console.log(err);
-            });
-        });
+const sqlite3 = require('sqlite3');
+const express = require('express');
+const bodyparser = require('body-parser');
+const app=express();
+const db = new sqlite3.Database('./database/movieonline.db');
+
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+app.get('/movie_detail.html',function (req ,res ) {
+    res.sendFile(__dirname + '/HTML/'+'movie_detail.html');
+});
+
+app.get ('/searchMovie',function (req,res) {
+    let movie_name =  req.body.search_name;
+    db.all("select * from Movies WHERE MovieName='"+movie_name+"'", function (err, result) {
+        if (!err) {
+            console.log(result);
+            res.send(result);
+        }
+        else
+            console.log(err);
+    });
+});
+
+db.all("select * from MovieComment", function (err, result) {
+    if (!err) {
+        console.log(result);
+    }
+    else
+        console.log(err);
+});
+
+db.all("select * from MovieDetails", function (err, result) {
+    if (!err) {
+        console.log(result);
+    }
+    else
+        console.log(err);
+});
+
+let server=app.listen(8080,function () {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("监听:%s%s",host,port);
+})
