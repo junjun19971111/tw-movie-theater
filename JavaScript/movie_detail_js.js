0c0e915a3c60f5a9f55080cf21bd62fc596cf150
Christine233 <1040551136@qq.com>
2017-08-02 17:00:54 +0800

    tset

--- JavaScript/movie_detail_js.js
+++ JavaScript/movie_detail_js.js
@@ -6,10 +6,20 @@ function printSearchResult(){
         let movieInfo=JSON.parse(data);
         let movie_name=document.getElementsByClassName("movie_name");
         for(let i=0;i<movie_name.length;i++){
-            movie_name.innerHTML=movieInfo[0].MovieName;
+            movie_name[i].innerHTML=movieInfo[0].MovieName;
         }
-        let movie_img=document.getElementById("movie_img").setAttribute("src", movieInfo.ImgUrl);
+
+        let movie_img=document.getElementById("movie_img").setAttribute("src", movieInfo[0].ImgUrl);
+
         let director=document.getElementById("director");
         director.innerHTML=movieInfo[0].Direcrtor;
+        let writers=document.getElementById("writers");
+        writers.innerHTML=movieInfo[0].writers;
+        let stars=document.getElementById("stars");
+        stars.innerHTML=movieInfo[0].stars;
+        let type=document.getElementById("type");
+        type.innerHTML=movieInfo[0].type;
+        let areas=document.getElementById("areas");
+        areas.innerHTML=movieInfo[0].areas;
     });
 }
