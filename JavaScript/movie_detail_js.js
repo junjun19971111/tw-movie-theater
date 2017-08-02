6f5edcd20d54316248072ba566376a063f10beb1
taoweicn <twchn@live.com>
2017-08-02 16:41:07 +0800

    modify

--- JavaScript/movie_detail_js.js
+++ JavaScript/movie_detail_js.js
@@ -3,6 +3,12 @@ document.getElementById("search_btn").addEventListener('click', printSearchResul
 function printSearchResult(){
     let keywords=document.getElementById("search").search_keywords;
     $.get(`/search_movie?search_keywords=${keywords}`, function (data, status) {
-        alert(data.length + status);
+        let movieInfo=JSON.parse(data);
+        let movie_name=document.getElementsByClassName("movie_name");
+        for(let i=0;i<movie_name.length;i++){
+            movie_name.innerHTML=movieInfo[0].MovieName;
+        }
+        let director=document.getElementById("director");
+        director.innerHTML=movieInfo[0].Direcrtor;
     });
 }
